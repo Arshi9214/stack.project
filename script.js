2235a43a53c68b9901f23bd8228368cda12f7bf0
AyeshaSk08 <engrayesha2008@gmail.com>
2025-08-19 01:12:37 +0530

    update

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hello, Stack project!");
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:1,
+        itemsDesktop:[1000,1],
+        itemsDesktopSmall:[979,1],
+        itemsTablet:[768,1],
+        pagination:true,
+        navigation:false,
+        slideSpeed:1000,
+        singleItem:true,
+        autoPlay:true
+    });
+});
